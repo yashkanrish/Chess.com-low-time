3d3da222235514583ffd4f7b93d2c1330310d2bd
yash sharma <yashkanrish@gmail.com>
2021-08-30 00:08:13 +0530

    adding remaining files

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({
+        name: "Jack"
+    });
+});
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+
+		chrome.scripting.executeScript({
+			target: { tabId: tabId },
+			files: ["./foreground.js"]
+		})
+			.then(() => {
+				console.log("INJECTED THE FOREGROUND SCRIPT.");
+			})
+			.catch(err => console.log(err));
+    }
+});
